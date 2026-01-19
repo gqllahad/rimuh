e69b0643684290b10fc7ddfe5840774db3b677e6
Inigo Joints <madrigalinigojones@gmail.com>
2026-01-19 15:23:40 +0800

    Wow

--- rimuh.js
+++ rimuh.js
@@ -1,4 +1,21 @@
 
+
+const images = document.querySelectorAll(".album img");
+const lightbox = document.getElementById("lightbox");
+const lightboxImg = document.getElementById("lightbox-img");
+
+images.forEach(img => {
+    img.addEventListener("click", () => {
+        lightboxImg.src = img.src;
+        lightbox.style.display = "flex";
+    });
+});
+
+lightbox.addEventListener("click", () => {
+    lightbox.style.display = "none";
+});
+
+
 new QRCode(document.getElementById("qrcode"), {
     text: "https://gqllahad.github.io/rimuh/rimuh.html",
     width: 200,
