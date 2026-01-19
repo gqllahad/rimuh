943118bd7cf30e53d9aa7d6ecf25579ee9d44a5e
Inigo Joints <madrigalinigojones@gmail.com>
2026-01-19 20:42:04 +0800

    hola

--- rimuh.js
+++ rimuh.js
@@ -45,9 +45,17 @@ const outer = document.getElementById("outer-container");
 const left = document.querySelector(".book-left");
 const right = document.querySelector(".book-right");
 
+
 const flowerBlooms = document.getElementById("flower-blooms");
 
 const title = document.getElementById("albumTitle");
+const items = document.getElementById("albumItems");
+const rimuh = document.getElementById("rimuh");
+
+const heart = document.getElementById("viewAll");
+let scale = 1;             
+const maxScale = 15;         
+const increment = 1.5;    
 
 flower.addEventListener("click", () => {
   flower.classList.add("bloom");
@@ -65,8 +73,13 @@ flower.addEventListener("click", () => {
 
       setTimeout(() => {
         title.style.display = "block"; 
+        items.style.display = "flex";
+        heart.style.display = "block";
+
         setTimeout(() => {
-          title.classList.add("show");   
+          title.classList.add("show");
+          items.classList.add("show");
+          heart.classList.add("show");   
         }, 50); 
       }, 1000);
 
@@ -79,7 +92,6 @@ function createBloomPetal() {
   const petal = document.createElement("div");
   petal.classList.add("bloom-petal");
 
-  // Random position near the book
   const x = (Math.random() - 0.5) * 400; // horizontal spread
   const y = (Math.random() - 0.5) * 400; // vertical spread
   const rot = Math.random() * 360 + "deg";
@@ -88,13 +100,11 @@ function createBloomPetal() {
   petal.style.setProperty("--y", y + "px");
   petal.style.setProperty("--rot", rot);
 
-  // random starting position near center
   petal.style.left = "50%";
   petal.style.top = "50%";
 
   flowerBlooms.appendChild(petal);
 
-  // remove after animation finishes
   setTimeout(() => {
     petal.remove();
   }, 3000);
@@ -103,6 +113,35 @@ function createBloomPetal() {
 flower.addEventListener("click", () => {
   for (let i = 0; i < 20; i++) {
     setTimeout(createBloomPetal, i * 900);
+  }
+});
+
+heart.addEventListener("click", () => {
+  if (!heart.classList.contains("explode")) {
+    scale += increment;
+    heart.style.transform = `rotate(-45deg) scale(${scale})`;
+
+    if (scale >= maxScale) {
+      heart.classList.add("explode");
+
+      setTimeout(() => {
+        heart.classList.remove("explode");
+        scale = 1;
+
+        items.classList.remove("show");
+        title.classList.remove("show");
+        
+        items.style.display = "none";
+        title.style.display = "none";
+
+        setTimeout(() => {
+            heart.style.transform = `rotate(-45deg) scale(${scale})`;
+            rimuh.classList.add("show");
+    
+    }, 500);
+        
+      }, 800); 
+    }
   }
 });
 
