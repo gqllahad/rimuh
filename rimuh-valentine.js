4bf4415d14426fb0aaa8667dc12f4efb36b06ebe
Inigo Joints <madrigalinigojones@gmail.com>
2026-01-23 13:29:40 +0800

    Rimuh Valentine

---
+++ rimuh-valentine.js
@@ -0,0 +1,8 @@
+new QRCode(document.getElementById("qrcode"), {
+    text: "https://gqllahad.github.io/rimuh/rimuh.html",
+    width: 200,
+    height: 200,
+    colorDark: "#cf3d81",
+    colorLight: "#a11313",
+    correctLevel: QRCode.CorrectLevel.H
+  });
