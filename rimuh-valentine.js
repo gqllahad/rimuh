97203e6912892ab8766379b28c6ee989a9fa5f68
Inigo Joints <madrigalinigojones@gmail.com>
2026-01-26 17:53:46 +0800

    Added some new animations and calendar to set date

--- rimuh-valentine.js
+++ rimuh-valentine.js
@@ -4,15 +4,24 @@ const press_yes = document.getElementById("yess");
 
 const hero = document.getElementById("hero");
 const hero_message = document.getElementById("hero-mess");
+const hero_comm = document.getElementById("hero-comm");
 
 const imageContainer = document.querySelector(".image-container");
+
+const daysContainer = document.getElementById("calendarDays");
+const monthYear = document.getElementById("monthYear");
+
+let date = new Date(2026, 1, 1);
+const minDate = new Date(2026, 0, 1);
+
+let selectedDate = null;
 
 const images = [
   "s7.gif",
   "s5.gif",
   "s3.gif",
   "s6.gif",
-  "s10.jpg",
+  "Untitled design (1).gif",
   "s9.gif",
   "s4.gif"
 ];
@@ -25,6 +34,8 @@ let dec_scale = 1.6;
 const maxScale = 15;         
 const increment = 1;    
 const decrement = 0.1;
+
+
 
 
 window.addEventListener("load", () => {
@@ -68,8 +79,105 @@ press_yes.addEventListener("click", () => {
     press_yes.classList.add("explode-valen");
     
     hero.style.display = "none";
-    document.querySelector(".hero-message").classList.add("show");
-})
+
+    document.querySelector(".hero-commercial").classList.add("show");
+
+    setTimeout(() => {
+      hero_comm.style.display = "none";
+      document.querySelector(".hero-message").classList.add("show");
+    },3100)
+
+    
+});
+
+function renderCalendar() {
+  daysContainer.innerHTML = "";
+  const year = date.getFullYear();
+  const month = date.getMonth();
+
+  const prevBtn = document.getElementById("prev");
+  const testPrev = new Date(date);
+
+  testPrev.setMonth(testPrev.getMonth() - 1);
+
+  prevBtn.disabled = testPrev < minDate;
+  prevBtn.style.opacity = prevBtn.disabled ? "0.4" : "1";
+  prevBtn.style.cursor = prevBtn.disabled ? "not-allowed" : "pointer";
+
+
+  monthYear.textContent = date.toLocaleDateString("en-US", {
+    month: "long",
+    year: "numeric"
+  });
+
+  const firstDay = new Date(year, month, 1).getDay();
+  const lastDate = new Date(year, month + 1, 0).getDate();
+  
+
+  for (let i = 0; i < firstDay; i++) {
+    daysContainer.appendChild(document.createElement("div"));
+  }
+
+  for (let day = 1; day <= lastDate; day++) {
+    const dayDiv = document.createElement("div");
+    dayDiv.textContent = day;
+
+    const today = new Date();
+    if (
+      day === today.getDate() &&
+      month === today.getMonth() &&
+      year === today.getFullYear()
+    ) {
+      dayDiv.classList.add("today");
+    }
+
+    if (
+    selectedDate &&
+    day === selectedDate.getDate() &&
+    month === selectedDate.getMonth() &&
+    year === selectedDate.getFullYear()
+  ) {
+    dayDiv.classList.add("selected");
+  }
+
+    dayDiv.addEventListener("click", () => {
+      document
+        .querySelectorAll(".calendar-days div")
+        .forEach(d => d.classList.remove("selected"));
+
+      dayDiv.classList.add("selected");
+
+      selectedDate = new Date(year, month, day);
+      
+      console.log(`Selected: ${year}-${month + 1}-${day}`);
+    });
+
+    daysContainer.appendChild(dayDiv);
+  }
+}
+
+document.getElementById("prev").onclick = () => {
+
+  const previousMonth = new Date(date);
+  previousMonth.setDate(1); 
+  previousMonth.setMonth(previousMonth.getMonth() - 1);
+
+  if (previousMonth < minDate) return;
+
+  date = previousMonth;
+  renderCalendar();
+};
+
+document.getElementById("next").onclick = () => {
+   const nextMonth = new Date(date);
+  nextMonth.setDate(1);  
+  date.setMonth(date.getMonth() + 1);
+  renderCalendar();
+};
+
+renderCalendar();
+
+
 
 new QRCode(document.getElementById("qrcode"), {
     text: "https://gqllahad.github.io/rimuh/rimuh.html",
